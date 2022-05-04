acd7ce98751a9093bee9451a98955a09f713ca7f
Gustavo Fernandes <gustavofernandes.f@outlook.com>
2022-05-04 17:09:46 -0300

    problema resolvido

--- 1064.py
+++ 1064.py
@@ -1,10 +1,15 @@
-arr = []
+valores = []
+positivos = []
+soma_positivos = 0
 
-soma = 0 
 for i in range(6):
-    n = float(input())
-    print(arr[i])
-    if n >= 0:
-        arr.append(n)
+    valor = float(input())
+    valores.append(valor)
+    if valores[i] > 0:
+        positivos.append(valores[i])
+        soma_positivos = soma_positivos + valores[i] 
+        
+media = soma_positivos / len(positivos)
 
-print(arr)
+print("{} valores positivos".format(len(positivos)))
+print(media)
